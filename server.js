8d3a497d10a41b8a3884667c06026617b14b8972
박륜 <parkryun@baglyun-ui-MacBookAir.local>
2024-01-18 05:20:21 +0900

    UPDATE: server.js & CREATE: router file, alarm.provide.js, alarm.sql.js

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const path = require("path")
 const app = express()
 const port = 3000
 
+const alarmApi = require("./router/alarm.js")
 
 // app.use ---------------------------------------------------
 app.use(express.json())
+app.use("/alarm", alarmApi)
 
 // app.get ---------------------------------------------------
 app.get("/", (req, res) => {
