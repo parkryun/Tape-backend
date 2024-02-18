a88cd43e5be46eed5a8941937c1c31bd4fdbd126
박륜 <parkryun@baglyun-ui-MacBookAir.local>
2024-02-18 21:48:23 +0900

    MODIFY

--- data/database.js
+++ data/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
+    host: 'localhost',
+    database: 'Tape',
+    user: 'root',
+    password: 'qkrfbs12!',
 });
 
-module.exports = pool;
+module.exports = pool; 
